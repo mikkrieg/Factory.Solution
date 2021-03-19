e7bbac83f81a202adfc7e90e3c39e4bfe7b41f74
Mikey Kriegel <mikkrieg@gmail.com>
2021-03-19 14:03:19 -0700

    Complete edit/delete routes and views

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -54,5 +54,48 @@ namespace Factory.Controllers
           .FirstOrDefault(engineer => engineer.EngineerId == id);
         return View(thisEngineer);
     }
+
+    public ActionResult Edit(int id)
+    {
+      var thisEngineer = _db.Engineers.FirstOrDefault(engineer => engineer.EngineerId == id);
+      ViewBag.MachineId = new SelectList(_db.Machines, "MachineId", "Model");
+      return View(thisEngineer);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Engineer engineer, int MachineId)
+    {
+        if (MachineId != 0)
+        {
+          _db.EngineerMachine.Add(new EngineerMachine() { MachineId = MachineId, EngineerId = engineer.EngineerId });
+        }
+        _db.Entry(engineer).State = EntityState.Modified;
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+        var thisEngineer = _db.Engineers.FirstOrDefault(engineer => engineer.EngineerId == id);
+        return View(thisEngineer);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+        var thisEngineer = _db.Engineers.FirstOrDefault(engineer => engineer.EngineerId == id);
+        _db.Engineers.Remove(thisEngineer);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public ActionResult DeleteMachine(int joinId)
+    {
+        var joinEntry = _db.EngineerMachine.FirstOrDefault(entry => entry.EngineerMachineId == joinId);
+        _db.EngineerMachine.Remove(joinEntry);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
   }
 }
